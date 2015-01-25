a6dd776c6e85f29a6718cdd3ddab3b2cfab87d9d
Elio E. Duarte <elio.esteves.duarte@gmail.com>
2015-01-25 18:17:31 -0200

    update code to most recent interface

--- tomate_gtk/application.py
+++ tomate_gtk/application.py
@@ -4,13 +4,9 @@ import logging
 
 import dbus.mainloop.glib
 from gi.repository import Gdk
-from tomate.application import application_factory, BaseApplication
-from tomate.base import ConnectSignalMixin
-from tomate.plugin import AddViewInstancePluginManager
+from tomate.application import application_factory, Application
+from tomate.base import AutoConnectSignalMixin
 from tomate.utils import setup_logging
-from yapsy.ConfigurablePluginManager import ConfigurablePluginManager
-from yapsy.PluginManager import PluginManagerSingleton
-from yapsy.VersionedPluginManager import VersionedPluginManager
 
 from .utils import parse_options
 from .widgets.window import Window
@@ -18,32 +14,15 @@ from .widgets.window import Window
 logger = logging.getLogger(__name__)
 
 
-class GtkApplication(ConnectSignalMixin,
-                     BaseApplication):
+class GtkApplication(AutoConnectSignalMixin,
+                     Application):
+
+    view_class = Window
 
     signals = (
         ('app_exit', 'exit'),
         ('session_duration_changed', 'on_configuration_changed'),
     )
-
-    def __init__(self, bus, view, **kwargs):
-        super(GtkApplication, self).__init__(bus, view)
-
-        self.connect_signals()
-
-        PluginManagerSingleton.setBehaviour([
-            AddViewInstancePluginManager,
-            VersionedPluginManager,
-            ConfigurablePluginManager,
-        ])
-
-        manager = PluginManagerSingleton.get()
-        manager.setPluginPlaces(self.profile.get_plugin_paths())
-        manager.setPluginInfoExtension(self.PLUGIN_EXT)
-        manager.setConfigParser(self.profile.config_parser,
-                                self.profile.write_config)
-        manager.setViewInstance(self.view)
-        manager.collectPlugins()
 
     def on_configuration_changed(self, sender=None, **kwargs):
         section = kwargs.get('section')
@@ -66,7 +45,7 @@ def main():
 
         dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 
-        app = application_factory(GtkApplication, Window(), options=options)
+        app = application_factory(GtkApplication, options=options)
 
         app.start()
 
