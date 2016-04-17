f7ac164cfe3284a16429a28be7ebcb049d2a5ac6
Elio E. Duarte <eduarte@thoughtworks.com>
2016-04-17 13:20:15 -0300

    Using py.test in test_appmenu

--- test/test_appmenu.py
+++ test/test_appmenu.py
@@ -1,33 +1,27 @@
 from __future__ import unicode_literals
 
-import unittest
-
-import six
 from mock import Mock
+from tomate.graph import graph
 from wiring import FactoryProvider, SingletonScope, Graph
 
-from tomate.graph import graph
 from tomate_gtk.widgets.appmenu import Appmenu, AppmenuModule
 
 
-class TestAppmenu(unittest.TestCase):
+def test_module():
+    assert AppmenuModule.providers.keys() == ['view.appmenu']
 
-    def test_module(self, *args):
-        six.assertCountEqual(self, ['view.appmenu'], AppmenuModule.providers.keys())
+    AppmenuModule().add_to(graph)
 
-        AppmenuModule().add_to(graph)
+    provider = graph.providers['view.appmenu']
 
-        provider = graph.providers['view.appmenu']
+    assert isinstance(provider, FactoryProvider)
+    assert provider.scope == SingletonScope
 
-        self.assertIsInstance(provider, FactoryProvider)
-        self.assertEqual(provider.scope, SingletonScope)
+    assert provider.dependencies == {'about': 'view.about',
+                                     'preference': 'view.preference',
+                                     'graph': Graph}
 
-        self.assertDictEqual({'about': 'view.about', 'preference': 'view.preference', 'graph': Graph},
-                             provider.dependencies)
+    graph.register_factory('view.preference', Mock)
+    graph.register_factory('view.about', Mock)
 
-        graph.register_factory('view.preference', Mock)
-        graph.register_factory('view.about', Mock)
-
-        appmenu = graph.get('view.appmenu')
-
-        self.assertIsInstance(appmenu, Appmenu)
+    assert isinstance(graph.get('view.appmenu'), Appmenu)
