161c3c9ab66e9c57a2a84dc3601397215c7d76d2
Elio E. Duarte <eduarte@thoughtworks.com>
2016-04-17 13:48:06 -0300

    Using py.test in test_menu

--- test/test_preference_dialog.py
+++ test/test_preference_dialog.py
@@ -1,8 +1,5 @@
 from __future__ import unicode_literals
 
-import unittest
-
-import six
 from mock import Mock
 from tomate.graph import graph
 from wiring import FactoryProvider, SingletonScope
@@ -12,47 +9,44 @@ from tomate_gtk.dialogs.preference import (ExtensionStack, PreferenceDialog,
                                            TimerDurationStack)
 
 
-class TestAboutDialog(unittest.TestCase):
+def test_module():
+    config = Mock(**{'get_int.return_value': 25})
+    graph.register_instance('tomate.config', config)
+    graph.register_factory('tomate.plugin', Mock)
+    graph.register_factory('view.preference.extension', ExtensionStack)
+    graph.register_factory('view.preference.duration', TimerDurationStack)
 
-    def test_module(self, *args):
-        providers = [
-            'view.preference',
-            'view.preference.extension',
-            'view.preference.duration',
-        ]
+    providers = [
+        'view.preference',
+        'view.preference.extension',
+        'view.preference.duration',
+    ]
 
-        six.assertCountEqual(self, providers, PreferenceDialogModule.providers.keys())
+    assert PreferenceDialogModule.providers.keys() == providers
 
-        PreferenceDialogModule().add_to(graph)
+    PreferenceDialogModule().add_to(graph)
 
-        # Extension Stack
-        provider = graph.providers['view.preference.extension']
-        self.assertIsInstance(provider, FactoryProvider)
-        self.assertEqual(provider.scope, SingletonScope)
-        self.assertDictEqual({'config': 'tomate.config', 'plugin': 'tomate.plugin'},
-                             provider.dependencies)
+    # Extension Stack
+    provider = graph.providers['view.preference.extension']
 
-        # Duration Stack
-        provider = graph.providers['view.preference.duration']
-        self.assertIsInstance(provider, FactoryProvider)
-        self.assertEqual(provider.scope, SingletonScope)
-        self.assertDictEqual({'config': 'tomate.config'}, provider.dependencies)
+    assert isinstance(provider, FactoryProvider)
+    assert provider.scope == SingletonScope
+    assert provider.dependencies == {'config': 'tomate.config', 'plugin': 'tomate.plugin'}
 
-        # Preference Dialog
-        provider = graph.providers['view.preference']
+    # Duration Stack
+    provider = graph.providers['view.preference.duration']
 
-        self.assertIsInstance(provider, FactoryProvider)
-        self.assertEqual(provider.scope, SingletonScope)
+    assert isinstance(provider, FactoryProvider)
+    assert provider.scope == SingletonScope
+    assert provider.dependencies == {'config': 'tomate.config'}
 
-        self.assertDictEqual({'extension': 'view.preference.extension',
-                              'duration': 'view.preference.duration'},
-                             provider.dependencies)
+    # Preference Dialog
+    provider = graph.providers['view.preference']
 
-        config = Mock(**{'get_int.return_value': 25})
-        graph.register_instance('tomate.config', config)
-        graph.register_factory('tomate.plugin', Mock)
-        graph.register_factory('view.preference.extension', ExtensionStack)
-        graph.register_factory('view.preference.duration', TimerDurationStack)
+    assert isinstance(provider, FactoryProvider)
+    assert provider.scope == SingletonScope
+    assert provider.dependencies == {'extension': 'view.preference.extension',
+                                     'duration': 'view.preference.duration'}
 
-        preference = graph.get('view.preference')
-        self.assertIsInstance(preference, PreferenceDialog)
+    preference = graph.get('view.preference')
+    assert isinstance(preference, PreferenceDialog)
