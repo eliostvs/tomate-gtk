1e9dea4b0a9e8d8f901f34363548fad1f1562e0b
Elio Duarte <eduarte@thoughtworks.com>
2018-09-16 09:56:39 -0300

    The layout is cleaner without the border.

--- tomate_gtk/widgets/timerframe.py
+++ tomate_gtk/widgets/timerframe.py
@@ -1,12 +1,11 @@
 import logging
 
 from gi.repository import Gtk
-from wiring import SingletonScope, inject
-from wiring.scanning import register
-
 from tomate.constant import State
 from tomate.event import Subscriber, Events, on
 from tomate.utils import format_time_left
+from wiring import SingletonScope, inject
+from wiring.scanning import register
 
 DEFAULT_TIME_LEFT = 25 * 60
 
@@ -15,29 +14,20 @@ logger = logging.getLogger(__name__)
 
 @register.factory('view.timerframe', scope=SingletonScope)
 class TimerFrame(Subscriber):
-    @inject(taskentry='view.taskentry')
-    def __init__(self, taskentry):
-        self.task_entry = taskentry
+    @inject(task_entry='view.taskentry')
+    def __init__(self, task_entry):
+        self.task_entry = task_entry
 
-        self.widget = Gtk.Frame(
-            margin_bottom=2,
+        self.timer_label = Gtk.Label(margin_bottom=14)
+
+        self.widget = Gtk.Box(
             margin_left=12,
             margin_right=12,
-            margin_top=12,
-            shadow_type=Gtk.ShadowType.IN,
+            orientation=Gtk.Orientation.VERTICAL,
         )
 
-        self.timer_label = Gtk.Label(justify=Gtk.Justification.CENTER,
-                                     expand=True,
-                                     vexpand=True,
-                                     halign=Gtk.Align.FILL,
-                                     valign=Gtk.Align.FILL)
-
-        box = Gtk.VBox()
-        box.pack_start(self.timer_label, True, True, 0)
-        box.pack_start(self.task_entry.widget, False, False, 0)
-
-        self.widget.add(box)
+        self.widget.pack_start(self.timer_label, False, False, 0)
+        self.widget.pack_start(self.task_entry.widget, False, False, 0)
 
     @on(Events.Timer, [State.changed])
     @on(Events.Session, [State.stopped, State.changed])
@@ -52,4 +42,3 @@ class TimerFrame(Subscriber):
     def time_left_markup(time_left):
         markup = '<span font="60">{}</span>'.format(format_time_left(time_left))
         return markup
-
