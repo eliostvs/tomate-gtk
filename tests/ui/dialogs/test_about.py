e1d786675bd4c239703fad7ee90bde6001e4b946
Elio Esteves Duarte <elio.esteves.duarte@gmail.com>
2021-04-11 09:40:58 -0300

    test(about): a more precise SUT name

--- tests/ui/dialogs/test_about.py
+++ tests/ui/dialogs/test_about.py
@@ -2,36 +2,36 @@ import pytest
 from gi.repository import Gtk
 from wiring.scanning import scan_to_graph
 
-from tests.conftest import refresh_gui
 from tomate import __version__
+from tomate.ui.testing import refresh_gui
 
 
 @pytest.fixture
-def subject(graph, config):
+def about(graph, config):
     graph.register_instance("tomate.config", config)
     scan_to_graph(["tomate.ui.dialogs.about"], graph)
 
     return graph.get("tomate.ui.about")
 
 
-def test_module(graph, subject):
-    assert graph.get("tomate.ui.about") is subject
+def test_module(graph, about):
+    assert graph.get("tomate.ui.about") is about
 
 
-def test_dialog_info(subject):
-    assert subject.get_comments() == "Tomate Pomodoro Timer (GTK+ Interface)"
-    assert subject.get_copyright() == "2014, Elio Esteves Duarte"
-    assert subject.get_version() == __version__
-    assert subject.get_website() == "https://github.com/eliostvs/tomate-gtk"
-    assert subject.get_website_label() == "Tomate GTK on Github"
-    assert subject.get_license_type() == Gtk.License.GPL_3_0
-    assert subject.get_logo() is not None
+def test_dialog_info(about):
+    assert about.get_comments() == "Tomate Pomodoro Timer (GTK+ Interface)"
+    assert about.get_copyright() == "2014, Elio Esteves Duarte"
+    assert about.get_version() == __version__
+    assert about.get_website() == "https://github.com/eliostvs/tomate-gtk"
+    assert about.get_website_label() == "Tomate GTK on Github"
+    assert about.get_license_type() == Gtk.License.GPL_3_0
+    assert about.get_logo() is not None
 
 
-def test_close_dialog(subject, mocker):
-    subject.hide = mocker.Mock()
+def test_close_dialog(about, mocker):
+    about.hide = mocker.Mock()
 
-    subject.emit("response", 0)
+    about.emit("response", 0)
     refresh_gui()
 
-    subject.hide.assert_called_once()
+    about.hide.assert_called_once()
