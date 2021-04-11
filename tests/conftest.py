cb1e6127c989458028fa26bf09eb6d45582440ef
Elio Esteves Duarte <elio.esteves.duarte@gmail.com>
2021-04-11 09:38:17 -0300

    test(utilities): conftest should only have fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,28 +1,28 @@
 import os
-import time
 
 import blinker
 import pytest
 from gi.repository import Gtk
 from wiring import Graph
 
+from tomate.pomodoro import Config, PluginEngine, Session
+from tomate.ui import ShortcutEngine, Window
+
 TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
 
 
 @pytest.fixture()
 def session(mocker):
-    from tomate.pomodoro.session import Session
-
     return mocker.Mock(spec=Session)
 
 
 @pytest.fixture()
-def bus():
+def bus() -> blinker.Signal:
     return blinker.NamedSignal("test")
 
 
 @pytest.fixture()
-def graph():
+def graph() -> Graph:
     g = Graph()
     g.register_instance(Graph, g)
     return g
@@ -30,62 +30,22 @@ def graph():
 
 @pytest.fixture()
 def view(mocker):
-    return mocker.Mock(widget=mocker.Mock(Gtk.Window))
+    return mocker.Mock(spec=Window, widget=mocker.Mock(spec=Gtk.Window))
 
 
-def refresh_gui(delay: int = 0) -> None:
-    while Gtk.events_pending():
-        Gtk.main_iteration_do(False)
-    time.sleep(delay)
-
-
-@pytest.fixture()
-def config(bus, tmpdir):
-    from tomate.pomodoro.config import Config
-
-    instance = Config(bus)
+@pytest.fixture
+def config(bus: blinker.Signal, tmpdir) -> Config:
+    cfg = Config(bus)
     tmp_path = tmpdir.mkdir("tomate").join("tomate.config")
-    instance.config_path = lambda: tmp_path.strpath
-    return instance
+    cfg.config_path = lambda: tmp_path.strpath
+    return cfg
 
 
-@pytest.fixture()
-def shortcut_manager(config):
-    from tomate.ui.shortcut import ShortcutManager
-
-    return ShortcutManager(config)
+@pytest.fixture
+def shortcut_engine(config: Config) -> ShortcutEngine:
+    return ShortcutEngine(config)
 
 
-def assert_shortcut_called(shortcut_manager, shortcut, want=True, window=None):
-    if window is None:
-        window = Gtk.Window()
-
-    shortcut_manager.init(window)
-    key, mod = Gtk.accelerator_parse(shortcut)
-    assert Gtk.accel_groups_activate(window, key, mod) is want
-
-
-def create_session_end_payload(**kwargs):
-    from tomate.pomodoro.session import EndPayload, Type
-
-    defaults = {
-        "id": "1234",
-        "duration": 5 * 60,
-        "pomodoros": 1,
-        "type": Type.SHORT_BREAK,
-    }
-    defaults.update(kwargs)
-    return EndPayload(**defaults)
-
-
-def create_session_payload(**kwargs):
-    from tomate.pomodoro.session import Payload, Type
-
-    defaults = {
-        "id": "1234",
-        "duration": 25 * 60,
-        "pomodoros": 0,
-        "type": Type.POMODORO,
-    }
-    defaults.update(kwargs)
-    return Payload(**defaults)
+@pytest.fixture
+def plugin_engine(config: Config) -> PluginEngine:
+    return PluginEngine(config)
--- tomate/ui/testing.py
+++ tomate/ui/testing.py
@@ -1,8 +1,54 @@
+import time
 from collections import deque
 from functools import reduce
-from typing import Any, Callable, List
+from typing import Any, Callable, List, Optional
 
 from gi.repository import GLib, Gtk
+
+from tomate.pomodoro import SessionEndPayload, SessionPayload, SessionType
+from tomate.ui import Shortcut, ShortcutEngine
+
+
+def active_shortcut(shortcut_engine: ShortcutEngine, shortcut: Shortcut, window: Optional[Gtk.Window] = None) -> bool:
+    if window is None:
+        window = Gtk.Window()
+
+    shortcut_engine.init(window)
+    key, mod = Gtk.accelerator_parse(shortcut.value)
+    return Gtk.accel_groups_activate(window, key, mod)
+
+
+def create_session_end_payload(**kwargs) -> SessionEndPayload:
+    defaults = {
+        "id": "1234",
+        "duration": 5 * 60,
+        "pomodoros": 1,
+        "type": SessionType.SHORT_BREAK,
+    }
+    defaults.update(kwargs)
+    return SessionEndPayload(**defaults)
+
+
+def run_loop_for(seconds: int = 1) -> None:
+    GLib.timeout_add_seconds(seconds, Gtk.main_quit)
+    Gtk.main()
+
+
+def refresh_gui(delay: int = 0) -> None:
+    while Gtk.events_pending():
+        Gtk.main_iteration_do(False)
+    time.sleep(delay)
+
+
+def create_session_payload(**kwargs) -> SessionPayload:
+    defaults = {
+        "id": "1234",
+        "duration": 25 * 60,
+        "pomodoros": 0,
+        "type": SessionType.POMODORO,
+    }
+    defaults.update(kwargs)
+    return SessionPayload(**defaults)
 
 
 class GtkWidgetNotFound(Exception):
@@ -14,16 +60,9 @@ Filter = Callable[[Any], bool]
 
 class Q:
     @staticmethod
-    def name(name: str) -> Filter:
+    def props(name: str, value: Any) -> Filter:
         def select(w: Gtk.Widget) -> bool:
-            return w.get_name() == name
-
-        return select
-
-    @staticmethod
-    def title(title: str) -> Filter:
-        def select(widget) -> bool:
-            return widget.get_title() == title
+            return getattr(w.props, name) == value
 
         return select
 
@@ -70,13 +109,6 @@ class TV:
         return tree_view.get_model()
 
     @staticmethod
-    def rows(*columns: int) -> Callable[[Gtk.TreeStore], List[Any]]:
-        def mapper(tree_store: Gtk.TreeStore) -> List[Any]:
-            return [[row[column] for column in columns] for row in tree_store]
-
-        return mapper
-
-    @staticmethod
     def column(fn: Filter) -> Callable[[Gtk.TreeView], List[Gtk.TreeViewColumn]]:
         def select(tree_view: Gtk.TreeView) -> List[Gtk.TreeViewColumn]:
             return [column for column in tree_view.get_columns() if fn(column)][0]
@@ -84,13 +116,8 @@ class TV:
         return select
 
     @staticmethod
-    def cell_renderer(column=0):
+    def cell_renderer(position: int) -> Callable[[Gtk.TreeViewColumn], Gtk.CellRenderer]:
         def select(tree_column: Gtk.TreeViewColumn) -> Gtk.CellRenderer:
-            return tree_column.get_cells()[column]
+            return tree_column.get_cells()[position]
 
         return select
-
-
-def run_loop_for(seconds: int = 1) -> None:
-    GLib.timeout_add_seconds(seconds, Gtk.main_quit)
-    Gtk.main()
