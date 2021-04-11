afd1efeb6f96127bf1e3e3615cf6181e0bcc00f3
Elio Esteves Duarte <elio.esteves.duarte@gmail.com>
2021-04-11 10:15:50 -0300

    test(countdown): import from the public API and a more precise SUT name

--- tests/ui/widgets/test_countdown.py
+++ tests/ui/widgets/test_countdown.py
@@ -4,46 +4,42 @@ import xml.etree.ElementTree as ET
 import pytest
 from wiring.scanning import scan_to_graph
 
-from tests.conftest import create_session_payload
-from tomate.pomodoro.event import Events
-from tomate.pomodoro.timer import Payload as TimerPayload, format_time_left
+from tomate.pomodoro import Events, TimerPayload, format_time_left
+from tomate.ui.testing import create_session_payload
+from tomate.ui.widgets import Countdown
 
 
 @pytest.fixture
-def subject(graph, bus):
+def countdown(bus, graph) -> Countdown:
     graph.register_instance("tomate.bus", bus)
     scan_to_graph(["tomate.ui.widgets.countdown"], graph)
     return graph.get("tomate.ui.countdown")
 
 
-def test_module(graph, subject):
-    from tomate.ui.widgets import Countdown
-
+def test_module(countdown, graph):
     instance = graph.get("tomate.ui.countdown")
 
     assert isinstance(instance, Countdown)
-    assert instance is subject
+    assert instance is countdown
 
 
 @pytest.mark.parametrize("event", [Events.SESSION_INTERRUPT, Events.SESSION_CHANGE])
-def test_updates_countdown_when_session_state_changes(event, subject, bus):
+def test_updates_countdown_when_session_state_changes(event, bus, countdown):
     duration = random.randint(1, 100)
 
     bus.send(event, payload=create_session_payload(duration=duration))
 
-    assert subject.widget.get_text() == format_time(duration)
+    assert countdown.widget.get_text() == format_time(duration)
 
 
-def test_updates_countdown_when_timer_changes(subject, bus):
+def test_updates_countdown_when_timer_changes(bus, countdown):
     time_left = random.randint(1, 100)
 
     bus.send(Events.TIMER_UPDATE, payload=TimerPayload(time_left=time_left, duration=0))
 
-    assert subject.widget.get_text() == format_time(time_left)
+    assert countdown.widget.get_text() == format_time(time_left)
 
 
 def format_time(time_left: int) -> str:
-    from tomate.ui.widgets import Countdown
-
     markup = Countdown.timer_markup(format_time_left(time_left))
     return ET.fromstring(markup).text
