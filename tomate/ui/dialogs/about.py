7128784b4dc9fc3c85652fc579eff352a0f65a67
Elio Esteves Duarte <elio.esteves.duarte@gmail.com>
2023-06-10 21:42:46 +0200

    Bump version: 0.24.0 → 0.25.0

--- tomate/ui/dialogs/about.py
+++ tomate/ui/dialogs/about.py
@@ -17,7 +17,7 @@ class AboutDialog(Gtk.AboutDialog):
             modal=True,
             program_name="Tomate Gtk",
             title="Tomate Gtk",
-            version="0.24.0",
+            version="0.25.0",
             website="https://github.com/eliostvs/tomate-gtk",
             website_label="Tomate GTK on Github",
         )
